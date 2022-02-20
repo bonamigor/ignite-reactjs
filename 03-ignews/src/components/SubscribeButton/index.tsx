6c4678b6a5f44cc2da9c2808b86acaff0789338b
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2022-02-19 21:08:11 -0300

    App connected with Stripe API
    Connected to Stripe API to retrieve subscription price infos and applied SSR and SSG concepts, explaining differences between client-side rendering, SSR & SSG.

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,6 +1,10 @@
 import styles from './styles.module.scss'
 
-export function SubscribeButton() {
+interface SubscribeButtonPros {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonPros) {
   return ( 
     <button type="button" className={styles.subscribeButton}>
       Subscribe now
