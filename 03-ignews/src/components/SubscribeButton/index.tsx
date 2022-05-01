80553e99278a637c1654de55d8aa84b97f225870
Rafael Bonamigo <rafaelrbonamigo@gmail.com>
2022-05-01 18:14:01 -0300

    Created Subscribe functionality
    Subscribe functionality is working with stripe payment, saving data in fauna & redirecting to posts page (not created yet, next module)

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,3 +1,6 @@
+import { useSession, signIn } from 'next-auth/react';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonPros {
@@ -5,8 +8,34 @@ interface SubscribeButtonPros {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonPros) {
+  const { data: session } = useSession()
+
+  async function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({ sessionId: sessionId })
+    } catch (error) {
+      alert(error.message);
+    }
+
+  }
+
   return ( 
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+      type="button" 
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
